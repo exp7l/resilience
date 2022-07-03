4ce822fc1ab25a55ac3013057fd995984eaa1210
exp7l <107167107+exp7l@users.noreply.github.com>
2022-07-03 14:57:36 -0700

    IAccount

--- src/Account.sol
+++ src/Account.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Account {
-  
+import "ds-deed/deed.sol";
+
+import "./IAccount.sol";
+
+contract Account is IAccount, DSDeed {
+  function stake(uint accountId, address collateralType, uint amount) external {
+  }
+  function unstake(uint accountId, address collateralType, uint amount) external {
+  } 
 }
