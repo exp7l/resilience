144d883f9529c286ffe6228500943d95a52d983f
exp7l <107167107+exp7l@users.noreply.github.com>
2022-07-08 18:37:42 -0700

    Rename and remove

--- src/IAccount.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IAccount {
-  function stake(uint accountId, address collateral, uint amount) external;
-  function unstake(uint accountId, address collateral, uint amount) external;
-}
