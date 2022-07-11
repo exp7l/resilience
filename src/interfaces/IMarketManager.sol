664b4813748e1cdeecf3a360b023cb37fc2e32de
0xkomada <80441337+0xkomada@users.noreply.github.com>
2022-07-11 16:34:13 +0800

    refactor: renaming in marketManager

--- src/interfaces/IMarketManager.sol
+++ src/interfaces/IMarketManager.sol
@@ -22,15 +22,15 @@ interface IMarketManager {
     function liquidity(uint256 marketId) external returns (uint256);
 
     // fund's debt incurred by underwriting a `buy` for `resBTC`
-    function fundBalance(uint256 marketId, uint256 fundId)
+    function fundDebt(uint256 marketId, uint256 fundId)
         external
         returns (int256);
 
     // fund's debts across all markets
-    function totalFundBalance(uint256 marketId) external returns (int256);
+    function totalFundDebt(uint256 marketId) external returns (int256);
 
     // What should be the unit here?
-    // manager takes resUSD from a market, then update debt balances for the funds that setLiquidity to this market and proportionally by the liquidity the fund provided
+    // manager takes sUSD from a market, then update debt balances for the funds that setLiquidity to this market and proportionally by the liquidity the fund provided
     function deposit(uint256 marketId, uint256 amount) external;
 
     // amount: amount of synth to redeem.
