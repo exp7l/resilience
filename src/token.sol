cdb654a2088bebecd565ca83132bd2c27926b942
exp7l <107167107+exp7l@users.noreply.github.com>
2022-07-12 14:45:27 -0700

    reference

--- src/token.sol
+++ src/token.sol
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Reference: https://github.com/dapphub/ds-token/blob/master/src/token.sol
 pragma solidity >=0.4.23 <=0.8.15;
 
 import "./auth.sol";
