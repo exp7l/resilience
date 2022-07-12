9c4a29fd9db970134d00435ea1cd65948f3e0e55
exp7l <107167107+exp7l@users.noreply.github.com>
2022-07-12 15:34:02 -0700

    fund WIP

--- src/fund.sol
+++ src/fund.sol
@@ -5,49 +5,81 @@ import "./interfaces/erc20.sol";
 import "./rdb.sol";
 
 /*
-Scope:
-1. Manage asset deposit from deeds.
-2. Manage vaults.
-3. Track vaults to debt shares.
-4. Appointment of manager.
-5. Liquidity provision using USD from the vaults the fund manages.
+  Scope:
+  1. Manage vaults.
+  2 Appointment of manager.
+  3. Liquidity provision using USD from the vaults the fund manages.
 */
 
 contract Fund {
   //       fundId
-  mapping (uint   => Appointment)                                      public  appointments;
-  //       fundId           collateralType        
-  mapping (uint   => mapping(address       => FundingRecord))          public  fundings;
+  mapping (uint => Appointment) public  appointments;
+
+  //       marketId  weight
+  mapping (uint =>   uint)      public  positions;
+
+  RDB rdb;
 
   struct Appointment {
-	uint    fundId;
+    uint    fundId;
     address manager;
     address nomination;
   }
 
-  struct FundingRecord {
-    uint    fundId;
-    address collateralType;
-    uint    usdBalance;
+  constructor(address _rdb)
+  {
+    rdb = RDB(_rdb);
   }
 
-  RDB rdb;
-
-  constructor(address _rdb) {
-	rdb = RDB(_rdb);
-  }
-
-  function createFund(uint _requestedFundId, address _owner) external {
-    require(appointments[_requestedFundId].manager == address(0), "ERR_ID_TAKEN");
-    appointments[_requestedFundId] = Appointment({
-      fundId:      _requestedFundId,
+  function createFund(uint    _fundId,
+                      address _manager)
+    external
+  {
+    Appointment memory _a = appointments[_fundId];
+    require(_a.manager == address(0), "ERR_FUNDID_TAKEN");
+    require(_a.manager != address(1), "ERR_RENOUNCED");
+    appointments[_fundId] = Appointment({
+      fundId:      _fundId,
       manager:     msg.sender,
-      nomination:  address(0)
-    });
+      nomination:  address(0)});
   }
-  function rebalanceMarkets(uint fundId) external {}
-  function setFundPosition(uint fundId, uint[] calldata markets, uint[] calldata weights) external {}
-  function nominateFundOwner(uint fundId, address owner) external {}
-  function acceptFundOwnership(uint fundId) external {}
-  function renounceFundOwnership(uint fundId) external {}
+  
+  function rebalanceMarkets(uint fundId)
+    external
+  {
+  }
+  
+  function setFundPosition(uint            _fundId,
+                           uint[] calldata _markets,
+                           uint[] calldata _weights)
+    external
+  {
+  }
+  
+  function nominateFundOwner(uint    _fundId,
+                             address _manager)
+    external
+  {
+    Appointment storage _a = appointments[_fundId];
+    require(msg.sender == _a.manager, "ERR_AUTH");
+    _a.nomination = _manager;
+  }
+
+  function acceptFundOwnership(uint _fundId)
+    external
+  {
+    Appointment storage _a = appointments[_fundId];
+    require(msg.sender == _a.nomination, "ERR_AUTH");
+    _a.manager    = msg.sender;
+    _a.nomination = address(0);
+  }
+  
+  function renounceFundOwnership(uint _fundId)
+    external
+  {
+    Appointment storage _a = appointments[_fundId];
+    require(msg.sender == _a.manager, "ERR_AUTH");
+    _a.manager    = address(1);
+    _a.nomination = address(0);
+  }
 }
